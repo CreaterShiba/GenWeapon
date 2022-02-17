2995f6a016361b1fe582fdadda8ede97fbf9dbc5
shiba <createshiba@gmail.com>
2022-02-18 01:51:46 +0900

    Qiita用プロジェクト追加

---
+++ Source/Genshin/GenshinGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GenshinGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GENSHIN_API AGenshinGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
